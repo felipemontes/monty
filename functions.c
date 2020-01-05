5d6746ab5ed5e67a9a73fcff5fc862e6d79bee98
Felipe <felipe_londono1@hotmail.com>
2020-01-05 20:11:31 +0000

    [ADD]: created pint function

---
+++ functions.c
@@ -0,0 +1,91 @@
+#include "monty.h"
+int global;
+/**
+ * push - add a element to the stack
+ * @stack: pointer to the stack
+ * @line_number: line number
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+
+	if (stack == NULL)
+	{
+		printf("L%d: invalid stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		printf("Error: malloc failed\n");
+		_free(stack, line_number);
+		exit(EXIT_FAILURE);
+	}
+	new->n = global;
+	new->prev = NULL;
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
+}
+/**
+ * pall - prints elements of the stack
+ * @stack: pointer to the stack
+ * @line_number: line number
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (stack == NULL)
+	{
+		printf("L%d: invalid stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+/**
+ * pop - removes the top element of a stack
+ * @stack: pointer to the stack
+ * @line_number: line number
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL)
+	{
+		printf("L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->next != NULL)
+	{
+		*stack = (*stack)->next;
+		global = (*stack)->n;
+		free((*stack)->prev);
+		(*stack)->prev = NULL;
+	}
+	else
+	{
+		free(*stack);
+		*stack = NULL;
+	}
+}
+/**
+ * pop - removes the top element of a stack
+ * @stack: pointer to the stack
+ * @line_number: line number
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+
+}
