261b6ecc2a59ccf4327b1b3b1f669f57bbfa03e7
Felipe <felipe_londono1@hotmail.com>
2020-01-05 22:38:25 +0000

    [FIX]: free memory

--- functions.c
+++ functions.c
@@ -73,7 +73,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (stack == NULL || *stack == NULL)
 	{
 		printf("L%d: can't pint, stack empty\n", line_number);
-		_free(stack, line_number);
+		_freed(*stack);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
